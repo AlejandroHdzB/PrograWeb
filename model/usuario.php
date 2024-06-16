11d9c8a332f0c7c4d12732b7a7b16591a6eac134
Alexa Contreras <akikoalexamma80@gmail.com>
2024-06-16 13:27:51 -0600

    Actualizacion de la vista de administrador, operaciones select y delete

--- model/usuario.php
+++ model/usuario.php
@@ -1,7 +1,15 @@
 <?php
 class Usuarios extends Orm{
     function __construct(PDO $connection){
-        parent::__construct('usuarios',$connection);
+        parent::__construct('id','usuarios',$connection);
+    }
+
+    function getNombreAdmin() {
+        $sql = "SELECT CONCAT(nombre, ' ', apellidos) AS nombreCompleto FROM usuarios WHERE rol = 'ADMIN'";
+        $stm = $this->db->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetch(PDO::FETCH_ASSOC);
+        return $result['nombreCompleto'];
     }
 }
 ?>
