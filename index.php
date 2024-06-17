68b36742dc13724a32f04b389e648073daf81a1f
Eddys912 <eddy912dev@gmail.com>
2024-06-16 18:01:02 -0600

    se agrega la seccion de registro y se corrigen rutas

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="resources/icono.ico" type="image/x-icon">
+    <link rel="icon" href="resources/img/icono.ico" type="image/x-icon">
     <link rel="shortcut icon" href="resources/icono.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="resources/index.css" rel="stylesheet">
     <title>Home</title>
 </head>
+
 <body>
     <div class="jumbotron header-img">
         <div class="container">
@@ -20,11 +22,12 @@
                 </div>
                 <nav class="nav col justify-content-end">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="formLogin.php">Iniciar sesión</a>
-                    <a class="nav-link" href="">Registrar</a>
+                    <a class="nav-link" href="#">Iniciar sesión</a>
+                    <a class="nav-link" href="Register/formRegister.php">Registrarse</a>
                 </nav>
-            </div>           
+            </div>
         </div>
     </div>
 </body>
+
 </html>
